b6d3fcb9554a44d39369d1dab4ca5ccf4a440227
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-11-16 01:49:27 +0900

    Load .env if env not set
    fixup! Load .env if env not set

--- bin/setup.php
+++ bin/setup.php
@@ -6,11 +6,14 @@ use BEAR\Dotenv\Dotenv;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-(new Dotenv())->load(dirname(__DIR__));
+if (! getenv('MA_DB_DSN')) {
+    (new Dotenv())->load(dirname(__DIR__));
+}
 
 chdir(dirname(__DIR__));
 passthru('chmod 775 var/tmp');
 passthru('chmod 775 var/log');
+
 // db
 $pdo = new PDO('mysql:host=' . getenv('MA_DB_HOST'), getenv('MA_DB_USER'), getenv('MA_DB_PASS'));
 $pdo->exec('CREATE DATABASE IF NOT EXISTS ' . getenv('MA_DB_NAME'));
