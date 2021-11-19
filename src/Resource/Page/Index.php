d3661400a0798711b053ead4b18d88338a8eb2a3
Akihito Koriyama <akihito.koriyama@gmail.com>
2021-11-19 13:40:50 +0900

    Revert "Delete tmp files on setup"
    This reverts commit a379d0c18ad1c4bf991425b45f7f02992f0bf3c4.

--- src/Resource/Page/Index.php
+++ src/Resource/Page/Index.php
@@ -6,9 +6,6 @@ namespace MaAlps\MaAlps\Resource\Page;
 
 use BEAR\Resource\ResourceObject;
 
-/**
- * @codeCoverageIgnore
- */
 class Index extends ResourceObject
 {
     /** @var array{greeting: string} */
