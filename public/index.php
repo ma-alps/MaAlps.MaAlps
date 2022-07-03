7b374f2f0f35ad00976291d07449b03953dcb4be
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-07-03 22:14:45 +0900

    apiコンテキストでホスト

--- public/index.php
+++ public/index.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 use MaAlps\MaAlps\Bootstrap;
 
 require dirname(__DIR__) . '/autoload.php';
-exit((new Bootstrap())(PHP_SAPI === 'cli-server' ? 'hal-app' : 'prod-hal-app', $GLOBALS, $_SERVER));
+exit((new Bootstrap())(PHP_SAPI === 'cli-server' ? 'hal-api-app' : 'prod-hal-api-app', $GLOBALS, $_SERVER));
