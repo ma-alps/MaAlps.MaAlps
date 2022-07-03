7d746c8b50bd2059ddbfcd1a08c1690df671ce40
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-07-03 18:35:00 +0900

    Enable HTTP hypermedia test

--- tests/Http/MyAlpsWorkflowTest.php
+++ tests/Http/MyAlpsWorkflowTest.php
@@ -5,13 +5,23 @@ declare(strict_types=1);
 namespace MaAlps\MaAlps\Http;
 
 //use BEAR\Dev\Http\HttpResource;
+use BEAR\Dev\Http\HttpResource;
 use MaAlps\MaAlps\Hypermedia\MyAlpsWorkflowTest as Workflow;
+
+use function dirname;
+use function passthru;
+use function sprintf;
 
 class MyAlpsWorkflowTest extends Workflow
 {
+    public static function setUpBeforeClass(): void
+    {
+        $setUp = sprintf('php %s', dirname(__DIR__, 2) . '/bin/setup.php');
+        passthru($setUp);
+    }
+
     protected function setUp(): void
     {
-        $this->markTestSkipped();
-//        $this->resource = new HttpResource('127.0.0.1:8080', __DIR__ . '/index.php', __DIR__ . '/log/workflow.log');
+        $this->resource = new HttpResource('127.0.0.1:8080', __DIR__ . '/index.php', __DIR__ . '/log/workflow.log');
     }
 }
