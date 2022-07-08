8d56d9140bc54ff70dc10012c13c9d0b63f569da
Akihito Koriyama <akihito.koriyama@gmail.com>
2022-07-08 12:03:19 +0900

    セマンティック例外のプロパティの値をコンストラクタに

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -4,15 +4,21 @@ declare(strict_types=1);
 
 namespace MaAlps\MaAlps\Exception;
 
+use function in_array;
+
 class RuntimeException extends \RuntimeException
 {
-    public function __toString(): string
+    public function __construct()
     {
         $message = '';
         foreach ($this as $key => $value) { // @phpstan-ignore-line
+            if (in_array($key, ['code', 'message', 'file', 'line'])) {
+                continue;
+            }
+
             $message .= "{$key}: {$value} ";
         }
 
-        return $message;
+        parent::__construct($message);
     }
 }
